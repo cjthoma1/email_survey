8ea8b137bbeb40cfa88cd3f23b0fa3c7ac9f7df4
Cray J Thomas Jr <cjthoma1@gmail.com>
2018-01-14 00:11:31 -0500

    Added stripe server and client in order for user to pay for credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {Link } from 'react-router-dom';
-
+import Payments from './Payments';
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -14,7 +14,11 @@ class Header extends Component {
           </li>
         );
       default:
-        return <li><a href= "/api/logout">Logout</a></li>
+        return [
+          <li key="1"><Payments/></li>,
+          <li style={{margin: '0 10px'}}key="3">Credits: {this.props.auth.credits}</li>,
+          <li key="2"><a href= "/api/logout">Logout</a></li>
+        ];
     }
   }
   render() {
